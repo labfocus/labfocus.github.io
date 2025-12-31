d7e3000a97bc34440f3433380ea09ae25488069d
Aldrick <geoffaldrick2010@gmail.com>
2025-12-31 11:07:59 +0800

    Delete admin.js

--- admin.js
+++
@@ -1,9 +0,0 @@
-function givePoints(email, pts){
-  let users = JSON.parse(localStorage.getItem("users")) || [];
-  const idx = users.findIndex(u=>u.email===email);
-  if(idx!==-1){
-    users[idx].points += pts;
-    localStorage.setItem("users", JSON.stringify(users));
-    alert(`${pts} points added to ${users[idx].name}`);
-  }
-}
